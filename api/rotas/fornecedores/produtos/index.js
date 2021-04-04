2a1ab3e56093498473fb604fa3a5e971a69de7ef
Leonardo Mendonça <leonardofsm@outlook.com>
2021-04-04 16:39:02 -0300

    criando produto

--- api/rotas/fornecedores/produtos/index.js
+++ api/rotas/fornecedores/produtos/index.js
@@ -1,11 +1,23 @@
 const roteador = require('express').Router({mergeParams: true})
 const Tabela = require('./TabelaProduto')
+const Produto = require('./Produto')
 
 roteador.get('/', async (request, response) => {
     const produtos = await Tabela.listar(request.params.idFornecedor)
     response.send(
         JSON.stringify(produtos)
     )
+})
+
+roteador.post('/', async (request, response) => {
+    const idFornecedor = request.params.idFornecedor
+    const body = request.body
+
+    const dados = Object.assign({}, body,  {fornecedor: idFornecedor})
+    const produto = new Produto(dados)
+    await produto.criar()
+    response.status(201)
+    response.send(produto)
 })
 
 module.exports = roteador
